67e1fc163d53a9a9f3c8012af0c3ea97ca722810
Philipp <philippkostyra@gmail.com>
2018-05-26 18:27:43 +0200

    Intial Spring Boot Setup
    Copy of Initalzr

---
+++ src/main/java/open/schoolmanagement/api/gateway/serviceapigateway/ServiceApiGatewayApplication.java
@@ -0,0 +1,12 @@
+package open.schoolmanagement.api.gateway.serviceapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceApiGatewayApplication.class, args);
+	}
+}
